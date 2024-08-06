f9b6755752f41e381432d20fa6244b904e5347aa
Jonathan Haugen <JHaugen@ssfcu.org>
2024-08-06 14:42:32 -0500

    Completed practical exercise for the AddEmployeeTest.

--- SeleniumPracticalExercise/Common/Utils.cs
+++ SeleniumPracticalExercise/Common/Utils.cs
@@ -20,5 +20,30 @@
 
             return new string(Enumerable.Repeat(chars, length).Select(s => s[Rnd.Next(s.Length)]).ToArray());
         }
+
+        /// <summary>
+        /// Returns a randomly generated int that can have up to as many digits as the given length
+        /// </summary>
+        /// <param name="length">The number of possible digits in the random int</param>
+        /// <returns>The generated int</returns>
+        public static int GenerateRandomInt(int length)
+        {
+            int generatedNumber = 0;
+            for (int i = 0; i < length; i++)
+                generatedNumber += Rnd.Next(10) * (int)Math.Pow(10, i);
+
+            return generatedNumber;
+        }
+
+        /// <summary>
+        /// Returns a randomly generated int as a string at the desired length by padding the leading
+        /// missing digits with 0 when the int value is shorter than the desired length
+        /// </summary>
+        /// <param name="length">The number of characters in the random string of numbers</param>
+        /// <returns>The generated string converted from an int</returns>
+        public static string GenerateRandomIntAsString(int length)
+        {
+            return $"{GenerateRandomInt(length)}".PadLeft(length,'0');
+        }
     }
 }
---
+++ SeleniumPracticalExercise/PageObjects/EmployeeListPage.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using SeleniumPracticalExercise.PageObjects.Common;
+
+namespace SeleniumPracticalExercise.PageObjects
+{
+    public class EmployeeListPage : BasePageLocal
+    {
+        private readonly By _AddBtnLocator = By.XPath("//div[@class='orangehrm-header-container']/button");
+
+        private readonly By _EmployeeIDSearch = By.XPath("//div[@class='oxd-form-row']//input[starts-with(@class,'oxd-input')]");
+
+        private readonly By _BtnSearchEmployeeData = By.CssSelector("button[type='submit']");
+        private readonly By _FoundEmployee = By.CssSelector("div.oxd-table-card");
+
+        public EmployeeListPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        /// <summary>
+        /// Clicks the Add button located on the Employee List page
+        /// </summary>
+        public void ClickAddButton()
+        {
+            Click(_AddBtnLocator);
+        }
+
+        /// <summary>
+        /// Searches for the given Employee ID using the search fields on the Employee List page
+        /// </summary>
+        /// <param name="employeeID">The Employee ID that is being searched for</param>
+        public void SearchForEmployeeByEmployeeID(string employeeID)
+        {
+            EditBoxSendKeysAndVerify(_EmployeeIDSearch, employeeID);
+            Click(_BtnSearchEmployeeData);
+            Click(_FoundEmployee);
+            WaitAfterAction(3000);
+        }
+    }
+}
